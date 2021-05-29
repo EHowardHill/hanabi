d2e4155b54a4484410879a5493df38df63bfd1aa
Ethan Hill <ethan.hill1999@outlook.com>
2021-05-29 17:17:05 -0500

    my process

--- process.py
+++ process.py
@@ -1,56 +1,20 @@
-import os, pprint
+import os
 
 text = """
 nook Z
     @a --type int16
     @b --type int16
     @c --type int16 --h
-    = c | d |
-        + a b
-        * 3 1
+    = c ( + a b )
 yes
+
 """
 
 text = text.replace('    ','\t ').replace('\n',' \n ')
 
 things = text.split(' ')[1:-1]
 
-class line():
-    oper = ""
-    factors = {}
-    references = {}
-    def __init__(self, oper="", factors={}, references={}):
-        self.oper = oper
-        self.factors = factors
-        self.references = references
-    def pprint(self):
-        for f in self.factors:
-            print('\t' + str(f) + '\t' + self.factors[f])
-
-class directive():
-    id = 0
-    lines = []
-    def __init__(self, id, lines=[]):
-        self.id = id
-        self.lines = lines
-    def pprint(self):
-        print('- id - ' + str(id))
-        try:
-            for line in self.lines:
-                line.pprint()
-        except Exception as e:
-            print(str(e))
-
-class nook():
-    name = ""
-    tags = []
-    directive = []
-    def __init__(self, name="", tags=None, directive= 0):
-        self.name = name
-        self.tags = tags
-        self.directive = directive
-    def pprint(self):
-        self.directive.pprint()
+line_count = 0
 
 t = 0
 size = len(things)
@@ -60,18 +24,14 @@ level = 0
 cur_level = 0
 level_offset = 0
 multiline = False
-cur_line = line()
-all_lines = directive(id=0)
+line_count = 0
 
 while t < size:
+    print("\t\t\t\t\t" + str(line_count))
     if things[t] == '\n':
         multiline = False
         cur_level = 0
         print("\n -newline- ")
-        all_lines.lines.append(cur_line)
-        cur_line = None
-        cur_line = line()
-        cur_line.pprint()
     elif things[t] == '\t':
         cur_level += 1
     elif things[t] == "|":
@@ -80,32 +40,21 @@ while t < size:
         else:
             multiline = True
             print("\n -newline- ")
-            all_lines.lines.append(cur_line)
-            cur_line = None
-            cur_line = line()
-            cur_line.pprint()
     else:
         if cur_level < level:
-            print("OUT OF SCOPE")
+            print(things[t] + "\tOUT OF SCOPE")
+            line_count += 1
+            level -= 1
         elif (cur_level - level_offset > level):
             print("NEW DIRECTIVE")
             print(things[t])
-            at = len(cur_line.factors)
-            cur_line.factors[at] = 'null'
-            cur_line.references[at] = 'ref'
         else:
+            # If relevant, print
             if (cur_level < level - 1): level_offset = 0
-            cur_line.factors[len(cur_line.factors)] = things[t]
             print(things[t] + '\t' + str(cur_level) + '\t' + str(level))
+
+            # If within a nook
             if things[t] in [":", "nook"] or (things[t+1] == '\n' and things[t] == "|"):
                 cur_level += 1
                 level += 1
     t += 1
-
-main = nook(
-    name="__main__",
-    tags=[],
-    directive=all_lines
-)
-
-#main.pprint()
